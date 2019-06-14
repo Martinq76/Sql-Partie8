0b63052f68568fa20149d62781a24bf957bb964e
Martinq76 <martin.queguiner.42@gmail.com>
2019-06-14 15:31:44 +0200

    Final partie8

--- index.sql
+++ index.sql
@@ -2,9 +2,9 @@ SOURCE 'bdd.sql';
 
 use development;
 
-SELECT * FROM languages LEFT JOIN frameworks ON languages.id = frameworks.languagesId;
+SELECT languages.name, frameworks.name FROM languages LEFT JOIN frameworks ON languages.id = frameworks.languagesId;
 
-SELECT * FROM languages INNER JOIN frameworks ON languages.id = frameworks.languagesId;
+SELECT languages.name, frameworks.name FROM languages INNER JOIN frameworks ON languages.id = frameworks.languagesId;
 
 SELECT languages.name, COUNT(frameworks.name) FROM languages LEFT JOIN frameworks ON languages.id = frameworks.languagesId GROUP BY languages.name HAVING COUNT(frameworks.name);
 
