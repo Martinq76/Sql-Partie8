5d604127bfe2d82ef5132961a3505bcf5d953379
Martinq76 <martin.queguiner.42@gmail.com>
2019-06-17 16:54:26 +0200

    fin partie8

--- index.sql
+++ index.sql
@@ -2,10 +2,10 @@ SOURCE 'bdd.sql';
 
 use development;
 
-SELECT languages.name, frameworks.name FROM languages LEFT JOIN frameworks ON languages.id = frameworks.languagesId;
+SELECT `languages.name`, `frameworks.name` FROM `languages` LEFT JOIN `frameworks` ON `languages.id` = `frameworks.languagesId`;
 
-SELECT languages.name, frameworks.name FROM languages INNER JOIN frameworks ON languages.id = frameworks.languagesId;
+SELECT `languages.name`, `frameworks.name` FROM `languages` INNER JOIN `frameworks` ON `languages.id` = `frameworks.languagesId`;
 
-SELECT languages.name, COUNT(frameworks.name) FROM languages LEFT JOIN frameworks ON languages.id = frameworks.languagesId GROUP BY languages.name HAVING COUNT(frameworks.name);
+SELECT `languages.name`, COUNT(`frameworks.name`) FROM `languages` LEFT JOIN `frameworks` ON `languages.id` = `frameworks.languagesId` GROUP BY `languages.name` HAVING COUNT(`frameworks.name`);
 
-SELECT languages.name, COUNT(frameworks.name) FROM languages LEFT JOIN frameworks ON languages.id = frameworks.languagesId GROUP BY languages.name HAVING COUNT(frameworks.name) > 3;
+SELECT `languages.name`, COUNT(`frameworks.name`) FROM `languages` LEFT JOIN `frameworks` ON `languages.id` = `frameworks.languagesId` GROUP BY `languages.name` HAVING COUNT(`frameworks.name`) > 3;
